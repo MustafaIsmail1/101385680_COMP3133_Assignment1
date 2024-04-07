df4614cc3f53a77596e14e2cba9234dd46284293
MustafaIsmail1 <125821910+MustafaIsmail1@users.noreply.github.com>
2024-04-06 21:43:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const resolvers = require('./resolvers')
 const mongoose = require('mongoose');
 
-async function Server() {
+// Apollo server
+async function startServer() {
     const app = express();
-    const apServer = new ApolloServer({
+    const apolloServer = new ApolloServer({
         typeDefs,
         resolvers
     });
 
-    await apServer.start();
-    apServer.applyMiddleware({ app });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
 
-    mongoose.set('strictQuery', true);
-    await mongoose.connect("mongodb+srv://mustafaismailmab:mumu123@cluster0.kmefq4m.mongodb.net/comp3133_assignment1?retryWrites=true&w=majority");
+    mongoose.set('strict', true); 
+    await mongoose.connect('mongodb+srv://mustafaismailmab:mumu123@cluster0.kmefq4m.mongodb.net/comp3133_assignment1?retryWrites=true&w=majority');
     console.log('Update: Database is connected to the project.');
-
-    const port = process.env.PORT || 8000;
-    app.listen(port, () =>
-        console.log(`Server is running on http://localhost:${port}/graphql`)
-    );
+    const port = 3000;
+    app.listen(port, () => console.log(`Server is up at port ${port}/graphql`));
 }
 
-Server();
+
+startServer();
